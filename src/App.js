2393983483da90857d2de32a16c955977842494b
daniarmakyev <daniarmakyev10@gmail.com>
2024-04-14 00:04:34 +0600

    compliteHeader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Header/Header";
 import "./components/MainStyles/style.css"
+import GoToDire from "./components/GoToDire/GoToDire";
+
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <GoToDire />
     </div>
   );
 }
---
+++ src/components/GoToDire/GoToDire.jsx
@@ -0,0 +1,16 @@
+import "./go-to-dire.css";
+
+function GoToDire({ }) {
+    return (
+        <div className="go-to-dire">
+            <div class="container">
+            <h1 class="text-center">Планируете сделать принт?
+                Вы пришли по адресу</h1>
+                <p class="text-center">Reklem - только оптовая работа высокого качества с индивидуальным подходом. Работаем с 2008 года.</p>
+                <button class="directory-btn">Перейти в каталог</button>
+            </div>
+        </div>
+    );
+  }                                              
+  
+  export default GoToDire;
