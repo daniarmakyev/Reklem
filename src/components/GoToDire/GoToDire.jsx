3d44c51ec03614f14d9b2f98bcc4608c1bb2dbc1
daniarmakyev <daniarmakyev10@gmail.com>
2024-04-14 14:08:18 +0600

    updateGTD

--- src/components/GoToDire/GoToDire.jsx
+++ src/components/GoToDire/GoToDire.jsx
@@ -1,14 +1,24 @@
 import "./go-to-dire.css";
 
+
 function GoToDire({ }) {
     return (
         <div className="go-to-dire">
             <div class="container">
-            <h1 class="text-center">Планируете сделать принт?
+            <h1 class="text-center">Планируете сделать <span>принт?</span>
                 Вы пришли по адресу</h1>
                 <p class="text-center">Reklem - только оптовая работа высокого качества с индивидуальным подходом. Работаем с 2008 года.</p>
-                <button class="directory-btn">Перейти в каталог</button>
+                <button class="directory-btn text-center">Перейти в каталог</button>
             </div>
+            <div className="backgorund-container">
+                <div className="background-inner">
+            <a href="#"><img src={require('../../images/cofta.png')} /></a>
+            <a href="#"><img src={require('../../images/crujka.png')} /></a>
+            <a href="#"><img src={require('../../images/fleshka.png')} /></a>
+            <a href="#"><img src={require('../../images/pen.png')} /></a>
+            <a href="#"><img src={require('../../images/electro.png')} /></a>
+            </div>
+               </div>
         </div>
     );
   }                                              
