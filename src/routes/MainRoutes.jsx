005b248753d1d1ef7788533da60f484a61e6de21
daniarmakyev <daniarmakyev10@gmail.com>
2024-04-27 12:21:46 +0600

    update

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import NoneFound from "../pages/NoneFoundPage/NoneFound";
 import MainLayout from "../MainLayout/MainLaout";
 import MainPage from "../pages/MainPage/MainPage";
-import Catalog from "../pages/CatalogPage/Catalog/Catalog";
-import AddCard from "../pages/CatalogPage/AddCard/AddCard";
-import EditCard from "../pages/CatalogPage/EditCard/EditCard";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="/" element={<MainPage/>} />
-        <Route path="/catalog" element={<Catalog/>}/>
-        <Route path="*" element={<NoneFound/>} />
-        <Route path="/addCard" element={<AddCard/>}/>
-        <Route path="/edit/:id" element={<EditCard/>}/>
       </Route>
     </Routes>
   );
